ed44f6c0b68afd2a98ee30466f04550c79705046
nietfeld@college.harvard.edu <eminietfeld@Emis-MacBook-Pro.local>
2013-11-19 14:26:49 -0500

    pushing new code to get senate data and house data

--- data_collection.py
+++ data_collection.py
@@ -1,8 +1,11 @@
 #configurations
-"""
 api_key = 'YOUR KEY HERE'
-movie_id = '770672122'  # toy story 3
-url = 'http://api.rottentomatoes.com/api/public/v1.0/movies/%s/reviews.json' % movie_id
+movie_id = '770672122'
+consumer_key = 'ylLqSZWgZ6CJlrJA4YmWkg'
+consumer_secret = 'jhdsOlMCXO4pgJdmO3ioGECPcdMDhb68WVxo5hkufA'
+
+
+#url = 'http://api.rottentomatoes.com/api/public/v1.0/movies/%s/reviews.json' % movie_id
 
 #these are "get parameters"
 options = {'review_type': 'top_critic', 'page_limit': 20, 'page': 1, 'apikey': api_key}
@@ -10,6 +13,4 @@ data = requests.get(url, params=options).text
 data = json.loads(data)  # load a json string into a collection of lists and dicts
 
 print json.dumps(data['reviews'][0], indent=2)  # dump an object into a json string
-
-
 """
